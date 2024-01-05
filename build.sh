8b3661c95d1ea6a54980aa2dfbad0ffa87bdd806
Ali-Abbas-Personal <ali.abbas.ssj1999@gmail.com>
2024-01-05 21:18:31 +0200

    [ESP-22] fix syntax error

--- build.sh
+++ build.sh
@@ -2,6 +2,5 @@ npm install
 mkdir -p android/app/src/main/assets
 npx react-native bundle --platform android --dev false --entry-file index.js --bundle-output android/app/src/main/assets/index.android.bundle --assets-dest android/app/src/main/res
 cd android && ./gradlew assembleRelease
-"
 
 echo "Build completed successfully."
