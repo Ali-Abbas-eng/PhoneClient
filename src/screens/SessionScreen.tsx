212dc506fbbd10848a61e30f05a9bc638f1478fb
Ali-Abbas-Personal <ali.abbas.ssj1999@gmail.com>
2024-03-18 09:05:30 +0300

    [ESP-23] receive audio and play it [WENT SUCCESSFULLY]

--- src/screens/SessionScreen.tsx
+++ src/screens/SessionScreen.tsx
@@ -1,10 +1,20 @@
 import { ActivityIndicator, Button, StyleSheet, View } from 'react-native';
-import { LoginScreenName, SocketIP } from '../constants/constants.tsx';
+import { SocketIP } from '../constants/constants.tsx';
 import { SessionScreenProps } from '../constants/types.tsx';
 import { useEffect, useRef, useState } from 'react';
+import AudioRecord from 'react-native-audio-record';
+import Sound from 'react-native-sound';
 
-export function SessionScreen({ route, navigation }: SessionScreenProps) {
+const audioOptions = {
+    sampleRate: 44100,
+    channels: 1,
+    bitsPerSample: 16,
+    wavFile: 'test.wav',
+};
+
+export function SessionScreen({ route }: SessionScreenProps) {
     const [socketInitialised, setSocketInitialised] = useState(false);
+    const [recording, setRecording] = useState(false);
     const { session } = route.params;
     const socketURL = SocketIP + session.socket_url;
     const ws = useRef<WebSocket | null>(null);
@@ -15,6 +25,7 @@ export function SessionScreen({ route, navigation }: SessionScreenProps) {
         ws.current.onopen = () => {
             console.log('Socket is open.');
             console.log('WebSocket readyState: ', ws.current?.readyState);
+            ws.current?.send(JSON.stringify({ start: 1 }));
             setSocketInitialised(true);
         };
 
@@ -29,9 +40,25 @@ export function SessionScreen({ route, navigation }: SessionScreenProps) {
             console.log('WebSocket readyState: ', ws.current?.readyState);
         };
 
-        ws.current.onmessage = (event: any) => {
+        ws.current.onmessage = async (event: any) => {
             console.log('Data: ', event.data);
             console.log('WebSocket readyState: ', ws.current?.readyState);
+            let data = JSON.parse(event.data);
+            if (data.audio) {
+                let sound = new Sound(data.audio, '', error => {
+                    if (error) {
+                        console.log('failed to load the sound', error);
+                    }
+                });
+
+                setTimeout(() => {
+                    sound.play(success => {
+                        if (!success) {
+                            console.log('Sound did not play');
+                        }
+                    });
+                }, 100);
+            }
         };
 
         return () => {
@@ -40,6 +67,20 @@ export function SessionScreen({ route, navigation }: SessionScreenProps) {
             }
         };
     }, [socketURL]);
+
+    const startRecording = async () => {
+        console.log('Starting recording..');
+        AudioRecord.init(audioOptions);
+        AudioRecord.start();
+        setRecording(true);
+    };
+
+    const stopRecording = async () => {
+        if (!recording) return;
+        let audioFile = await AudioRecord.stop();
+        console.log('Audio file stored at: ', audioFile);
+        setRecording(false);
+    };
 
     if (!socketInitialised) {
         // show a loading indicator while fetching the data
@@ -54,13 +95,8 @@ export function SessionScreen({ route, navigation }: SessionScreenProps) {
         <View
             style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
             <Button title="Start Conversation" onPress={() => {}} />
-            <Button
-                title="Start Recording"
-                onPress={() => {
-                    navigation.navigate(LoginScreenName);
-                }}
-            />
-            <Button title="Stop Recording" onPress={() => {}} />
+            <Button title="Start Recording" onPress={startRecording} />
+            <Button title="Stop Recording" onPress={stopRecording} />
         </View>
     );
 }
