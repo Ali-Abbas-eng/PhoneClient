43e07cd48b0eccc5ae429a2f2d9fbcce272009af
Ali-Abbas-Personal <ali.abbas.ssj1999@gmail.com>
2024-03-31 14:11:38 +0300

    [ESP-54] fix build issues, JPEG encoded image saved as . png renamed to .jpg

--- compile_and_run.sh
+++ compile_and_run.sh
@@ -1,31 +1,82 @@
 #!/bin/bash
 
-# Activate the Python virtual environment
-source ~/esp/bin/activate
+# Function to reset color
+reset_color() {
+    echo -e "\e[30m"
+}
 
-yarn install
-
-rm -rf android
-rm -rf ios
-react-native eject
-cd one-off-fixes
-# Run the Python script
-python3 replace.py
-cd ..
+# Trap the EXIT signal to always execute reset_color function
+trap reset_color EXIT
 
 # Install yarn packages
-cd android
+yarn install
+if [ $? -ne 0 ]; then
+    echo -e "\e[31mYARN INSTALL COMMAND FAILED"
+    exit 1
+fi
+echo -e "\e[32mYARN INSTALL COMMAND EXECUTED SUCCESSFULLY"
 
-./gradlew clean
-./gradlew build
+# Remove android and ios directories
+rm -rf android
+rm -rf ios
+if [ $? -ne 0 ]; then
+    echo -e "\e[31mREMOVING ANDROID AND IOS FILES FAILED"
+    exit 1
+fi
+
+echo -e "\e[32mANDROID AND IOS FILES WERE REMOVED SUCCESSFULLY"
+
+# Sleep for 2 seconds
+sleep 1
+
+# Eject react-native
+react-native eject
+if [ $? -ne 0 ]; then
+    echo -e "\e[31mREACT NATIVE EJECT COMMAND FAILED"
+    exit 1
+fi
+
+echo -e "\e[32mREACT NATIVE EJECT COMMAND EXECUTED SUCCESSFULLY"
+
+# Sleep for 2 seconds
+sleep 1
+
+# Run the Python script
+python3 replace.py
+if [ $? -ne 0 ]; then
+    echo -e "\e[31mONE-OFF FIX FILES FAILED TO APPLY"
+    exit 1
+fi
+
+echo -e "\e[32mONE-OFF FIX FILES WERE APPLIED SUCCESSFULLY"
+
+# Change directory to android
+cd android
+if [ $? -ne 0 ]; then
+    echo -e "\e[31mFAILED TO CHANGE DIRECTORY"
+    exit 1
+fi
 
 # Create necessary directories
 mkdir -p android/app/src/main/assets
+if [ $? -ne 0 ]; then
+    echo -e "\e[31mmkdir -p failed"
+    exit 1
+fi
+# Clean gradle
+./gradlew clean
+if [ $? -ne 0 ]; then
+    echo -e "\e[31mGRADLE CLEAN COMMAND FAILED"
+    exit 1
+fi
 
-# Bundle the React Native project
-npx react-native bundle --platform android --dev false --entry-file index.js --bundle-output android/app/src/main/assets/index.android.bundle --assets-dest android/app/src/main/res
+echo -e "\e[32mGRADLE CLEAN COMMAND EXECUTED SUCCESSFULLY"
 
-echo "Build completed successfully."
-
-# Run the Android application
-react-native run-android
+# Build gradle
+./gradlew build
+if [ $? -ne 0 ]; then
+    echo -e "\e[31mGRADLE BUILD COMMAND FAILED"
+    exit 1
+fi
+echo -e "\e[32mGRADLE BUILD COMMAND EXECUTED SUCCESSFULLY"
+echo -e "\e[32mBUILD COMPLETED SUCCESSFULLY"
--- replace.py
+++ replace.py
@@ -4,11 +4,13 @@ import shutil
 def get_first_node(path):
     return os.path.split(path)[0].split(os.sep)[0]
 
-one_off_fixes_source = 'fixes'
-one_off_fixes_destination = '..'
+one_off_fixes_source = os.path.join('one-offs', 'fixes')
+one_off_fixes_destination = ''
 
 for root, dirs, files in os.walk(one_off_fixes_source):
     for file in files:
         fix_file = os.path.join(root, file)
-        destination_file = fix_file.replace(get_first_node(fix_file), one_off_fixes_destination)
+        if (not os.path.isfile(fix_file)):
+            continue
+        destination_file = fix_file.replace(one_off_fixes_source + os.path.sep, one_off_fixes_destination)
         shutil.copy(fix_file, destination_file)
