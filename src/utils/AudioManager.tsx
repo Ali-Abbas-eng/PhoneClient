63c9a2526a69269810e8d6506342975cb5ce11bf
Ali-Abbas-Personal <ali.abbas.ssj1999@gmail.com>
2024-04-08 06:17:53 +0300

    [ESP-55] added audio management functionality

--- src/utils/AudioManager.tsx
+++ src/utils/AudioManager.tsx
@@ -5,74 +5,174 @@ import AudioRecorderPlayer, {
     AVEncodingOption,
     AVModeIOSOption,
 } from 'react-native-audio-recorder-player';
+import { DocumentDirectoryPath, downloadFile } from 'react-native-fs';
+import Sound from 'react-native-sound';
 
-const audioRecorderPlayer = new AudioRecorderPlayer();
-import { DocumentDirectoryPath } from 'react-native-fs';
-import { readFile } from 'react-native-fs';
-const Sound = require('react-native-sound');
-export const playSound = (soundFile: string) => {
-    let sound = new Sound(soundFile, Sound.MAIN_BUNDLE, (error: any) => {
-        if (error) {
-            console.log('failed to load the sound', error);
-            return;
+export class AudioManagerAPI {
+    private audioRecorderPlayer: AudioRecorderPlayer;
+    private audioChunkRecorderPlayer: AudioRecorderPlayer;
+    private __isRecording: boolean;
+    private __isRecordingChunk: boolean;
+    private audioSet: {
+        AudioEncoderAndroid: AudioEncoderAndroidType;
+        AVNumberOfChannelsKeyIOS: number;
+        AVFormatIDKeyIOS: AVEncodingOption;
+        AudioSourceAndroid: AudioSourceAndroidType;
+        AVModeIOS: AVModeIOSOption;
+        AVEncoderAudioQualityKeyIOS: AVEncoderAudioQualityIOSType
+    };
+    private audioCount: number;
+    private __isStoppable: boolean;
+    private meteringEnabled: boolean;
+
+    constructor() {
+        this.meteringEnabled = false;
+        this.audioRecorderPlayer = new AudioRecorderPlayer();
+        this.audioChunkRecorderPlayer = new AudioRecorderPlayer();
+        this.__isRecording = false;
+        this.audioCount = 0;
+        this.__isRecordingChunk = false;
+        this.audioSet = {
+            AudioEncoderAndroid: AudioEncoderAndroidType.AAC,
+            AudioSourceAndroid: AudioSourceAndroidType.MIC,
+            AVModeIOS: AVModeIOSOption.measurement,
+            AVEncoderAudioQualityKeyIOS: AVEncoderAudioQualityIOSType.high,
+            AVNumberOfChannelsKeyIOS: 2,
+            AVFormatIDKeyIOS: AVEncodingOption.aac,
+        };
+        this.__isStoppable = false;
+    }
+
+    generateAudioFilePaths() {
+        const baseName = `${Math.floor(new Date().getTime() / 1000)}_${this.audioCount}`;
+        const audioFiles = {
+            full: `${DocumentDirectoryPath}/${baseName}.aac`,
+            chunk: `${DocumentDirectoryPath}/${baseName}_chunk.aac`,
         }
-        // loaded successfully
-        console.log(
-            `Duration in Seconds: ${sound.getDuration()}
-             Number of Channels: ${sound.getNumberOfChannels()}`,
-        );
+        return audioFiles;
+    }
 
-        // Play the sound with an onEnd callback
-        sound.play((success: boolean) => {
-            if (success) {
-                console.log('successfully finished playing');
+    async audioRecordInference(MINIMUM_AUDIO_LENGTH: number,
+                               MAXIMUM_AUDIO_LENGTH: number,
+                               AUDIO_CHUNK_LENGTH: number | null) {
+        this.__isRecording = true;
+        let result;
+        if (AUDIO_CHUNK_LENGTH){
+            // We are recording a chunk so that we can get a fast response
+            result = await this.startChunkRecording(AUDIO_CHUNK_LENGTH);
+        } else {
+            // We are recording the full audio
+            result = await this.startFullRecording(MINIMUM_AUDIO_LENGTH, MAXIMUM_AUDIO_LENGTH);
+        }
+        return result;
+    }
+
+    // Function to start recording a chunk of audio
+    async startChunkRecording(chunkLength: number){
+        console.log('Starting chunk recording...');
+        const filePath = this.generateAudioFilePaths()["chunk"]
+        try {
+            const uri = await this.audioChunkRecorderPlayer.startRecorder(
+                filePath,
+                this.audioSet,
+                this.meteringEnabled,
+            );
+            this.__isRecordingChunk = true;
+
+            // Stop recording after chunkLength seconds
+            setTimeout(() => {
+                if (this.__isRecordingChunk) {
+                    console.log('Stopping chunk recording...');
+                    this.audioChunkRecorderPlayer.stopRecorder();
+                    this.__isRecordingChunk = false;
+                }
+            }, chunkLength * 1000);
+
+            console.log('Chunk recording started successfully');
+            return { isRecordingChunk: true, audioPath: uri };
+        } catch (error) {
+            console.log('Failed to start chunk recording:', error);
+            return { isRecordingChunk: false, audioPath: '', error: error };
+        }
+    }
+
+    async startFullRecording(MINIMUM_AUDIO_LENGTH: number, MAXIMUM_AUDIO_LENGTH: number){
+        console.log('Started Recording...')
+        const filePath = this.generateAudioFilePaths()["full"]
+        try {
+            const uri = await this.audioRecorderPlayer.startRecorder(
+                filePath,
+                this.audioSet,
+                this.meteringEnabled,
+            );
+            this.__isRecording = true;
+
+            // Allow user to stop recording after MINIMUM_AUDIO_LENGTH seconds
+            setTimeout(() => {
+                this.__isStoppable = true;
+            }, MINIMUM_AUDIO_LENGTH * 1000);
+
+            // Stop recording automatically after MAXIMUM_AUDIO_LENGTH seconds
+            setTimeout(() => {
+                if (this.__isRecording) {
+                    this.stopRecording();
+                }
+            }, MAXIMUM_AUDIO_LENGTH * 1000);
+
+            return { isRecording: true, audioPath: uri };
+        } catch (error) {
+            console.log('Uh-oh! Failed to start recording:', error);
+            return { isRecording: false, audioPath: '', error: error };
+        }
+    }
+
+    async stopRecording() {
+        if (this.__isRecording && this.isStoppable()) {
+            try {
+                await this.audioRecorderPlayer.stopRecorder();
+                console.log('Stopped Recording!')
+                this.__isRecording = false;
+                this.__isStoppable = false;
+            } catch (error) {
+                console.log('Oops! Failed to stop recording:', error);
+            }
+        }
+    }
+
+    playSound(soundFile: string) {
+        let sound = new Sound(soundFile, Sound.MAIN_BUNDLE, (error: any) => {
+            if (error) {
+                console.log('failed to load the sound', error);
             } else {
-                console.log('playback failed due to audio decoding errors');
+                sound.play((success: boolean) => {
+                    if (success) {
+                        console.log('successfully finished playing');
+                    } else {
+                        console.log('playback failed due to audio decoding errors');
+                    }
+                });
             }
         });
-    });
-};
-
-const generateAudioName = () => {
-    return `${Math.floor(new Date().getTime() / 1000)}`;
-};
-
-export const startRecording = async () => {
-    const path = `${DocumentDirectoryPath}/${generateAudioName()}.aac`;
-    const audioSet = {
-        AudioEncoderAndroid: AudioEncoderAndroidType.AAC,
-        AudioSourceAndroid: AudioSourceAndroidType.MIC,
-        AVModeIOS: AVModeIOSOption.measurement,
-        AVEncoderAudioQualityKeyIOS: AVEncoderAudioQualityIOSType.high,
-        AVNumberOfChannelsKeyIOS: 2,
-        AVFormatIDKeyIOS: AVEncodingOption.aac,
-    };
-    const meteringEnabled = false;
-
-    try {
-        const uri = await audioRecorderPlayer.startRecorder(
-            path,
-            audioSet,
-            meteringEnabled,
-        );
-        return { isRecording: true, audioPath: uri };
-    } catch (error) {
-        console.log('Uh-oh! Failed to start recording:', error);
-        return { isRecording: false, audioPath: '', error: error };
     }
-};
 
-export const stopRecording = async (isRecording: boolean) => {
-    if (isRecording) {
-        try {
-            await audioRecorderPlayer.stopRecorder();
-            return { isRecording: false };
-        } catch (error) {
-            console.log('Oops! Failed to stop recording:', error);
-            return { isRecording: isRecording, error: error };
-        }
-    } else {
-        console.log('Not Recording to being with');
-        return { isRecording };
+    isStoppable(){
+        return this.__isStoppable;
     }
-};
+
+    isRecording() {
+        return this.__isRecording;
+    }
+
+    isRecordingChunk(){
+        return this.__isRecordingChunk
+    }
+
+    isRecordingSwitch(){
+        this.__isRecording = !this.__isRecording;
+    }
+
+    isRecordingChunkSwitch() {
+        this.__isRecordingChunk = !this.__isRecordingChunk;
+    }
+}
+
