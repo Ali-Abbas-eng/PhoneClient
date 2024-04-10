df7545b8a173a10c941525a30e98b234ea4fd3fa
Ali-Abbas-Personal <ali.abbas.ssj1999@gmail.com>
2024-04-10 10:23:53 +0300

    [ESP-55] incorporated the new classes' implementation into the SpeakingSessionManager component

--- src/components/SpeakingSessionManager.tsx
+++ src/components/SpeakingSessionManager.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useMemo } from 'react';
 import { Button, View } from 'react-native';
 import { SocketIP } from '../constants/constants.tsx';
 import { connect } from 'react-redux';
@@ -13,31 +13,20 @@ import {
 import { requestPermissions } from '../utils/PermissionsManager.tsx';
 import { PERMISSIONS } from 'react-native-permissions';
 import { notifyMessage } from '../utils/informationValidators.tsx';
-import { AudioManagerAPI } from '../utils/AudioManager.tsx';
-import {
-    initialiseWebSocket,
-    sendAudio,
-    startConversation,
-} from '../utils/WebSocketManager.tsx';
+import { EchoSessionManager } from '../utils/EchoSessionManager.tsx';
 
-const SpeakingSessionManager = ({
-    session,
-    webSocket,
-    receivedAudios,
-    echoTurn,
-    addReceivedAudios,
-    removeReceivedAudio,
-    setEchoTurn,
-}: any) => {
-    const audioManager = React.useMemo(() => new AudioManagerAPI(), []);
-    audioManager.registerOnStopRecordingCallback(async (filePath: string) => {
-        await sendAudio(webSocket, filePath);
-        console.log(`File at: ${filePath} is being sent to Echo server`);
-        audioManager.isPlayingSwitch();
-        // TODO: FIX: recorded file is not reachable.
-    });
-    const socketURL = SocketIP + session.socket_url;
-    const [sessionInitialised, setSessionInitialised] = useState(false);
+const SpeakingSessionManager = ({ session }: any) => {
+    const echoSessionManager = useMemo(
+        () =>
+            new EchoSessionManager(
+                SocketIP + session.socket_url,
+                10, // messageCountLimit
+                5, // minimumMessageLength
+                10, // maximumMessageLength
+                3, // chunkMessageLength
+            ),
+        [session.socket_url],
+    );
 
     // make sure all the required permissions are granted.
     useEffect(() => {
@@ -52,71 +41,15 @@ const SpeakingSessionManager = ({
     }, []);
 
     useEffect(() => {
-        webSocket.current = new WebSocket(socketURL);
-        initialiseWebSocket(webSocket, audioPath => {
-            // Dispatch the action to add the audio path to the global state
-            addReceivedAudios(audioPath);
-        });
-        startConversation(webSocket);
-    }, [addReceivedAudios, socketURL, webSocket]);
-
-    useEffect(() => {
-        // Handle the one-off case where echo starts the conversation with one message
-        if (echoTurn && !sessionInitialised) {
-            let audioFile = receivedAudios[0];
-            audioManager.playSound(audioFile).then(() => {
-                removeReceivedAudio(audioFile);
-                setSessionInitialised(true);
-                setEchoTurn(false);
-                audioManager.isPlayingSwitch();
-            });
-        } else if (echoTurn && sessionInitialised) {
-            // moving forward, echo will be sending two messages
-            // TODO: two stage audio send/receive functionality
-            console.log('Not Implemented yet');
-        }
-    }, [
-        audioManager,
-        echoTurn,
-        receivedAudios,
-        removeReceivedAudio,
-        sessionInitialised,
-        setEchoTurn,
-    ]);
-
-    useEffect(() => {
-        // Handle the one-off case where echo starts the conversation with one message
-        if (!echoTurn && !audioManager.isPlaying()) {
-            // it's the user's turn
-            console.log("It is not Echo's turn, we must record....");
-            audioManager
-                .startRecording(5, 10, 3)
-                .then(fullAudioInfo => {
-                    sendAudio(webSocket, fullAudioInfo.audioPath).then(() => {
-                        console.log('Complete Audio Sent');
-                        setEchoTurn(true);
-                    });
-                    console.log('Playing Recorded Audio....');
-                })
-                .catch(error => {
-                    console.error(error);
-                });
-            // TODO: prepare multi-stage request
-            /*
-             * record two audios
-             * send each one immediately when it's done recording
-             * when both audios are sent:
-             *   change echoTurn to true
-             */
-        }
-    }, [audioManager, echoTurn, setEchoTurn, webSocket]);
+        echoSessionManager.converse();
+    }, [echoSessionManager]);
 
     return (
         <View>
             <Button
                 title="Stop Recording"
                 onPress={() => {}}
-                disabled={!audioManager.isStoppable()}
+                disabled={!echoSessionManager.isAudioRecordSendable}
             />
         </View>
     );
