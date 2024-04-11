52c3636d6f9caffee02e0af3f12bd4c2760c3e8d
Ali-Abbas-Personal <ali.abbas.ssj1999@gmail.com>
2024-04-11 09:01:28 +0300

    [ESP-55] refactoring, context binding, removing unused code

--- src/screens/UI/Session.tsx
+++ src/screens/UI/Session.tsx
@@ -1,18 +1,20 @@
 import React, { useEffect, useMemo } from 'react';
 import { Button, View } from 'react-native';
-import { SessionScreenProps } from '../../constants/types.tsx';
+import { RootState, SessionScreenProps } from '../../constants/types.tsx';
 import { EchoSessionManager } from '../../utils/EchoSessionManager.tsx';
 import { SocketIP } from '../../constants/constants.tsx';
 import { PERMISSIONS } from 'react-native-permissions';
 import { requestPermissions } from '../../utils/PermissionsManager.tsx';
 import { notifyMessage } from '../../utils/informationValidators.tsx';
 import { useDispatch, useSelector } from 'react-redux';
-import { setWebSocket, setEchoSessionManager } from '../../redux/actions';
+import { setEchoSessionManager } from '../../redux/sessionSlice';
 
 export const Session: React.FC<SessionScreenProps> = ({ route }) => {
     const dispatch = useDispatch();
-    const webSocket = useSelector(state => state.webSocket);
-    const echoSessionManager = useSelector(state => state.echoSessionManager);
+    const webSocket = useSelector((state: RootState) => state.webSocket);
+    const echoSessionManager = useSelector(
+        (state: RootState) => state.echoSessionManager,
+    );
     const [start, setStart] = React.useState(false);
     const { session } = route.params;
 
@@ -20,7 +22,6 @@ export const Session: React.FC<SessionScreenProps> = ({ route }) => {
         if (webSocket) {
             const newEchoSessionManager = new EchoSessionManager(
                 SocketIP + session.socket_url,
-                webSocket,
                 10, // messageCountLimit
                 5, // minimumMessageLength
                 10, // maximumMessageLength
@@ -50,9 +51,9 @@ export const Session: React.FC<SessionScreenProps> = ({ route }) => {
                 title="Start Chat"
                 onPress={() => {
                     setStart(true);
-                    echoSessionManager.webSocketManager.startConversation();
+                    echoSessionManager?.webSocketManager?.startConversation();
                 }}
-                disabled={start && echoSessionManager?.webSocketManager}
+                disabled={start}
             />
             <Button
                 title="Stop Recording"
