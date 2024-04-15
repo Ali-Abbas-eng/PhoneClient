e85b20fac51d9cbb5c675978efc44a3998335343
Ali-Abbas-Personal <Ali.Abbas.ssj1999@gmail.com>
2024-04-15 14:52:13 +0300

    [ESP-55] moved back to using a session management component (EchoSessionManager)

--- src/screens/UI/Session.tsx
+++ src/screens/UI/Session.tsx
@@ -1,41 +1,29 @@
 import React, { useEffect, useState } from 'react';
-import { Button, View } from 'react-native';
+import { ActivityIndicator, View } from 'react-native';
 import { SessionScreenProps } from '../../constants/types.tsx';
-import { EchoSessionManager } from '../../utils/EchoSessionManager.tsx';
 import { SocketIP } from '../../constants/constants.tsx';
 import { PERMISSIONS } from 'react-native-permissions';
 import { requestPermissions } from '../../utils/PermissionsManager.tsx';
 import { notifyMessage } from '../../utils/informationValidators.tsx';
 import { WebSocketManager } from '../../utils/WebSocketManager.tsx';
 import { AudioManagerAPI } from '../../utils/AudioManager.tsx';
+import { EchoSessionManager } from '../../components/EchoSessionManager.tsx';
+import { styles } from '../../styles/styels.tsx';
 
 export const Session: React.FC<SessionScreenProps> = ({ route }) => {
-    const [start, setStart] = React.useState(false);
     const { session } = route.params;
-    const [echoSessionManager, setEchoSessionManager] =
-        useState<EchoSessionManager | null>(null);
+
+    const [webSocketManager, setWebSocketManager] =
+        useState<WebSocketManager | null>(null);
+    const [audioManager, setAudioManager] = useState<AudioManagerAPI | null>(
+        null,
+    );
 
     useEffect(() => {
+        setAudioManager(new AudioManagerAPI());
         const webSocket = new WebSocket(SocketIP + session.socket_url);
-        const webSocketManager = new WebSocketManager(webSocket);
-        const audioManager = new AudioManagerAPI();
-
-        setEchoSessionManager(
-            new EchoSessionManager(
-                audioManager,
-                webSocketManager,
-                10, // messageCountLimit
-                5, // minimumMessageLength
-                10, // maximumMessageLength
-                3, // chunkMessageLength
-            ),
-        );
+        setWebSocketManager(new WebSocketManager(webSocket));
     }, [session.socket_url]);
-
-    useEffect(() => {
-        console.log('The value of Echo Session Manager: ');
-        console.log(echoSessionManager);
-    }, [echoSessionManager]);
 
     useEffect(() => {
         const permissionGrants = requestPermissions([
@@ -50,32 +38,23 @@ export const Session: React.FC<SessionScreenProps> = ({ route }) => {
 
     useEffect(() => {
         return () => {
-            if (echoSessionManager) {
-                echoSessionManager.webSocketManager.webSocket.close();
-                setEchoSessionManager(null);
+            if (webSocketManager) {
+                webSocketManager.webSocket.close();
             }
         };
-    }, [echoSessionManager]);
-
-    return (
-        <View>
-            <Button
-                title="Start Chat"
-                onPress={() => {
-                    setStart(true);
-                    setTimeout(() => {
-                        echoSessionManager
-                            ? echoSessionManager.converse()
-                            : console.log(echoSessionManager);
-                    }, 1);
-                }}
-                disabled={start}
+    }, [webSocketManager]);
+    if (webSocketManager && audioManager) {
+        return (
+            <EchoSessionManager
+                webSocketManager={webSocketManager}
+                audioManager={audioManager}
             />
-            <Button
-                title="Stop Recording"
-                onPress={() => {}}
-                disabled={!echoSessionManager?.isAudioRecordSendable}
-            />
-        </View>
-    );
+        );
+    } else {
+        return (
+            <View style={styles.center}>
+                <ActivityIndicator size="large" color="#0000ff" />
+            </View>
+        );
+    }
 };
