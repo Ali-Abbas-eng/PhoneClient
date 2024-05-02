a355d21f6e681e63cafd1a4ac5dace48a9358355
Ali-Abbas-Personal <Ali.Abbas.ssj1999@gmail.com>
2024-05-02 10:54:24 +0300

    [ESP-55] created complex audio class to handle a list of files (chunks and non-chunks)

--- src/utils/AudioManager.tsx
+++ src/utils/AudioManager.tsx
@@ -12,6 +12,7 @@ export class AudioManagerAPI {
     private audioRecorderPlayer: AudioRecorderPlayer;
     private __isRecording: boolean;
     private __isPlaying: boolean;
+    private __audioBuffer: string[];
     private onStopRecordingCallback: (filePath: string) => void;
     private audioSet: {
         AudioEncoderAndroid: AudioEncoderAndroidType;
@@ -32,6 +33,7 @@ export class AudioManagerAPI {
         this.__isStoppable = false;
         this.__isPlaying = false;
         this.__isRecording = false;
+        this.__audioBuffer = [];
         this.onStopRecordingCallback = (_: string) => {};
         this.audioSet = {
             AudioEncoderAndroid: AudioEncoderAndroidType.AAC,
@@ -113,14 +115,17 @@ export class AudioManagerAPI {
                     .stopRecorder()
                     .then((fileURI: string) => {
                         this.onStopRecordingCallback(fileURI);
+                        this.__audioBuffer.push(fileURI);
                         this.audioRecorderPlayer.removeRecordBackListener();
                     });
             } catch (error) {}
             if (!isChunk) {
                 this.__isStoppable = false;
                 this.__isRecording = false;
+                return new ComplexAudioObject(this.__audioBuffer);
             }
         }
+        return;
     }
 
     playSound(soundFile: string, callback: { (): void; (): void } | undefined) {
@@ -158,5 +163,74 @@ export class AudioManagerAPI {
     cleanup() {
         this.audioRecorderPlayer.removeRecordBackListener();
         this.audioRecorderPlayer.removePlayBackListener();
+    }
+}
+
+export class ComplexAudioObject {
+    private audioFiles: any;
+    private totalDuration: number;
+    private currentFileIndex: number;
+    private isPlaying: boolean;
+    private soundObjects: any;
+    constructor(audioFiles: string[]) {
+        this.audioFiles = audioFiles; // Array of audio file paths
+        this.totalDuration = 0; // Total duration of all audio files
+        this.currentFileIndex = 0; // Index of the current audio file being played
+        this.isPlaying = false; // Is audio currently playing
+
+        // Initialize Sound objects for each audio file and calculate total duration
+        this.soundObjects = this.audioFiles.map((filePath: string) => {
+            let sound = new Sound(filePath, '', error => {
+                if (error) {
+                    console.log('Failed to load the sound', error);
+                    return;
+                }
+                // Update total duration
+                this.totalDuration += sound.getDuration();
+            });
+            return sound;
+        });
+    }
+
+    // Play function to play the audio files consecutively
+    play() {
+        if (this.currentFileIndex < this.soundObjects.length) {
+            this.isPlaying = true;
+            let sound = this.soundObjects[this.currentFileIndex];
+            sound.play((success: boolean) => {
+                if (success) {
+                    console.log(
+                        `Finished playing ${
+                            this.audioFiles[this.currentFileIndex]
+                        }`,
+                    );
+                    this.currentFileIndex++;
+                    this.play(); // Play the next audio file
+                } else {
+                    console.log('Playback failed due to audio decoding errors');
+                }
+            });
+        } else {
+            console.log('Finished playing all audio files');
+            this.isPlaying = false;
+            this.currentFileIndex = 0; // Reset the index if you want to replay the list later
+        }
+    }
+
+    // Function to stop the audio playback
+    stop() {
+        if (this.isPlaying) {
+            let sound = this.soundObjects[this.currentFileIndex];
+            sound.stop(() => {
+                console.log('Playback stopped');
+                this.isPlaying = false;
+                this.currentFileIndex = 0; // Reset the index
+            });
+        }
+    }
+
+    // Function to get the total duration of the audio list
+    getDuration() {
+        return this.totalDuration;
     }
 }
